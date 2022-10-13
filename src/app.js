f68327536000fb33b3c1e6f2baa8b47c7d823528
Your Name <lucasdosualdo@gmail.com>
2022-10-13 14:56:27 -0300

    create auth and transactions routes files

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ import express from "express";
 import joi from "joi";
 import cors from "cors";
 import dotenv from "dotenv";
-import { connection } from "./database.js";
+import { connection } from "./connection/database.js";
+import authRouters from "./routers/authRouters.js";
+import transactionsRouters from "./routers/transactionsRouters.js";
 
 dotenv.config();
 const app = express();
@@ -12,6 +14,9 @@ app.use(cors());
 app.use("/status", (req, res) => {
   res.send("server on");
 });
+
+app.use(authRouters);
+app.use(transactionsRouters);
 
 app.listen(process.env.PORT, () => {
   console.log("listening in port " + process.env.PORT);
